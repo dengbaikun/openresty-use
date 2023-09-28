7cd9f9e340c3aea03a1d88e89528a5a6c7a026f7
dengbaikun <dengbaikun@foxmail.com>
2023-09-28 13:49:56 +0800

    upadte

--- src/main/lua/HelloWorld.lua
+++ src/main/lua/HelloWorld.lua
@@ -1,3 +1,4 @@
+package.path ='F:/IdeaProject/openresty/openresty/src/main/lua/?.lua;;D:/luasocket-3.1.0/src/?.lua'
 local say = require("say")
 local msg = 'Hello World!!!!!!!!!'
 print(say.say(msg))
