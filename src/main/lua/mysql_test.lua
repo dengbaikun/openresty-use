755ecd704c30290a048bbb07656720a2bd4fd3da
dengbaikun <dengbaikun@foxmail.com>
2023-10-07 11:54:22 +0800

    redis

--- src/main/lua/mysql_test.lua
+++ src/main/lua/mysql_test.lua
@@ -4,54 +4,59 @@
 --- DateTime: 2023/9/28 16:43
 ---
 ---
-local mysql_db = require "mysql_db"
-local redis_db = require "redis_db"
+local MysqlPool = require "mysql_db"
+local RedisPool = require "redis_db"
 local cjson = require("cjson");
 local success_response = {}
 success_response["code"] = 200
 success_response["message"] = "成功"
 ngx.header.content_type = "application/json"
--- 创建数据库连接
-local red, err = redis_db.new()
+-- 创建 Redis 连接
+local red, err = RedisPool.new()
 if not red then
-    ngx.say("Failed to connect to the mysql_db: ", err)
+    ngx.say("Failed to connect to Redis: ", err)
     return
 end
-local resp,err = red:get('test_db=================')
-if  resp then
-    ngx.log(ngx.ERR,"get redis db")
-    success_response["data"] = cjson.decode(resp)
-    ngx.say(cjson.encode(success_response))
-    redis_db.close(red)
-    return
-end
--- 创建数据库连接
-local db, err = mysql_db.new()
-if not db then
-    ngx.say("Failed to connect to the mysql_db: ", err)
-    return
-end
--- 执行查询
-local sql = "SELECT * FROM users"
-local res, err = mysql_db.query(db, sql)
-if not res then
-    ngx.say("Failed to execute query: ", err)
+local redis_key = 'test_db================='
+local res, err = RedisPool.command(red, "get", redis_key)
+ngx.log(ngx.ERR, "get redis db :", res)
+if res == ngx.null then
+    -- 创建数据库连接
+    local db, err = MysqlPool.new()
+    if not db then
+        ngx.say("Failed to connect to the mysql_db: ", err)
+        return
+    else
+        ngx.log(ngx.ERR, "get mysql db ")
+        -- 执行查询
+        local sql = "SELECT * FROM users"
+        local res, err = MysqlPool.query(db, sql)
+        if not res then
+            ngx.say("Failed to execute query: ", err)
+        else
+            local data = res
+            res, err = RedisPool.command(red, "set", redis_key, cjson.encode(data))
+            res, err = RedisPool.command(red, "expire", redis_key, 10)
+            success_response["data"] = data
+            --数据响应类型JSON
+            ngx.say(cjson.encode(success_response))
+            --ngx.log(ngx.ERR,"res type :",type(res))
+            ---- 处理查询结果
+            --for i, row in ipairs(res) do
+            --    ngx.say("TYPE ",type(row))
+            --    ngx.log(ngx.ERR,"row:",cjson.encode(row))
+            --    ngx.log(ngx.ERR,"row concat:",table.concat(row))
+            --    ngx.say("Row ", i, "role: ", row['role'])
+            --    ngx.say("Row ", i, ": ", cjson.encode(row))
+            --end
+        end
+        --释放数据库连接
+        MysqlPool.close(db)
+        RedisPool.close(red)
+    end
 else
-    ngx.log(ngx.ERR,"get mysql db")
-    red:set('test_db=================', cjson.encode(res))
-    success_response["data"] = res
-    --数据响应类型JSON
+    ngx.log(ngx.ERR, "get redis db ")
+    success_response["data"] = cjson.decode(res)
     ngx.say(cjson.encode(success_response))
-    --ngx.log(ngx.ERR,"res type :",type(res))
-    ---- 处理查询结果
-    --for i, row in ipairs(res) do
-    --    ngx.say("TYPE ",type(row))
-    --    ngx.log(ngx.ERR,"row:",cjson.encode(row))
-    --    ngx.log(ngx.ERR,"row concat:",table.concat(row))
-    --    ngx.say("Row ", i, "role: ", row['role'])
-    --    ngx.say("Row ", i, ": ", cjson.encode(row))
-    --end
+    RedisPool.close(red)
 end
--- 释放数据库连接
-mysql_db.close(db)
-redis_db.close(red)
