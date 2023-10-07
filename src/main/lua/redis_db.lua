7b7af7ec6c8614ec67a6307c4bd2d2793d68e3da
dengbaikun <dengbaikun@foxmail.com>
2023-10-07 11:54:22 +0800

    redis

--- src/main/lua/redis_db.lua
+++ src/main/lua/redis_db.lua
@@ -8,44 +8,52 @@ local redis = require('resty.redis')
 local _M = {}  -- 创建一个模块表
 -- 定义连接池的配置
 local redis_config = {
-    host = "127.0.0.1",   -- Redis 服务器地址
-    port = 6379,          -- Redis 服务器端口
-    timeout = 1000,       -- 连接超时时间（毫秒）
+    host = "127.0.0.1", -- Redis 服务器地址
+    port = 6379, -- Redis 服务器端口
+    timeout = 1000, -- 连接超时时间（毫秒）
     max_idle_timeout = 60000, -- 连接在连接池中的最大空闲时间（毫秒）
-    pool_size = 100,       -- 连接池大小
+    pool_size = 100, -- 连接池大小
     password = "123456"
 }
 -- 创建数据库连接池
 function _M.new()
-    local db, err = redis.new()
-    if not db then
-        ngx.log(ngx.ERR, "failed to create mysql object: ", err)
-        return nil, err
-    end
+    local red = redis:new()
+    red:set_timeout(redis_config.timeout)  -- 设置超时时间（以毫秒为单位）
 
-    db:set_timeout(redis_config.timeout)  -- 设置超时时间（以毫秒为单位）
-
-    local ok, err = db:connect(redis_config.host, -- 数据库主机
+    local ok, err = red:connect(redis_config.host, -- 数据库主机
             redis_config.port -- 数据库端口
     )
 
     if not ok then
-        ngx.log(ngx.ERR, "failed to connect to database: ", err, ": ", errno, " ", sqlstate)
+        ngx.log(ngx.ERR, "failed to connect to Redis: ", err)
         return nil, err
     end
-    db:auth(redis_config.password)
-    return db, nil  -- 返回数据库连接对象
+    local ok, err = red:auth(redis_config.password)
+    if not ok then
+        ngx.log(ngx.ERR, "Redis auth fail: ", err)
+        return nil, err
+    end
+    return red, nil  -- 返回 Redis 连接对象
+end
+-- 执行 Redis 命令
+function _M.command(red, cmd, ...)
+    local res, err = red[cmd](red, ...)
+    if not res then
+        ngx.log(ngx.ERR, "failed to execute Redis command: ", err)
+        return nil, err
+    end
+
+    return res, nil  -- 返回命令执行结果
 end
 
-
 -- 释放连接
-function _M.close(db)
-    if not db then
+function _M.close(red)
+    if not red then
         return
     end
 
     --释放连接(连接池实现)
-    local ok, err = db:set_keepalive(redis_config.max_idle_timeout, redis_config.pool_size)
+    local ok, err = red:set_keepalive(redis_config.max_idle_timeout, redis_config.pool_size) -- 连接在连接池中最多保持 60000 秒
     if not ok then
         ngx.log(ngx.ERR, "set redis keepalive error : ", err)
     end
