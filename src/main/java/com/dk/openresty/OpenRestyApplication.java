2a1817d57f0c3b825da86d36d9890201325d4b58
dengbaikun <dengbaikun@foxmail.com>
2023-10-08 17:16:37 +0800

    代理 加解密数

---
+++ src/main/java/com/dk/openresty/OpenRestyApplication.java
@@ -0,0 +1,16 @@
+package com.dk.openresty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author DK
+ * @version V1.0.0
+ * @date 2023/10/8 16:54
+ */
+@SpringBootApplication
+public class OpenRestyApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(OpenRestyApplication.class, args);
+    }
+}
---
+++ src/main/java/com/dk/openresty/controller/TestController.java
@@ -0,0 +1,32 @@
+package com.dk.openresty.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author DK
+ * @version V1.0.0
+ * @date 2023/10/8 16:56
+ */
+@RestController("/test")
+@Slf4j
+public class TestController {
+
+
+    @RequestMapping
+    public Object index(@RequestBody String body) {
+        log.info("body=[{}]", body);
+        Map<String, Object> map = new HashMap<>();
+        map.put("code", 200);
+        map.put("msg", "SUCCESS");
+        map.put("data", body);
+        return map;
+    }
+
+}
