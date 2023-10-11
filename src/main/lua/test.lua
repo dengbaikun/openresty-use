84a0bfa03d7af137e9270644b2a92d250d7c1ce3
dengbaikun <dengbaikun@foxmail.com>
2023-10-11 16:58:37 +0800

    mysql工具优化

--- src/main/lua/test.lua
+++ src/main/lua/test.lua
@@ -1,10 +1,18 @@
 local redis = require "redis_tool"
 local red = redis:new()
-
-local ok, err = red:set("dog", "an animal")
-if not ok then
-    ngx.say("failed to set dog: ", err)
-    return
-end
-
-ngx.say("set result: ", ok)
+--ngx.log(ngx.ERR,"red.host",str(red.host))
+--local ok, err = red:set("dog", "an animal")
+--if not ok then
+--    ngx.say("failed to set dog: ", err)
+--    return
+--end
+--red:t1()
+ngx.say(red.host)
+ngx.say(red._VERSION)
+ngx.say(redis.port)
+red:init_pipeline()
+red:set("dog", "an animal")
+ngx.say(red:get('dog'))
+red:commit_pipeline()
+--red:t1()
+--ngx.say("set result: ", ok)
